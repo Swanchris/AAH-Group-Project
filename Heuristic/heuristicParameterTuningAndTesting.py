79ba268583e880fc85a851386ad85432ec4c8813
AnthonyRas <51532102+AnthonyRas@users.noreply.github.com>
2020-10-20 17:55:17 +1100

    Update and rename heuristicExperiment.py to heuristicParameterTuningAndTesting.py

---
+++ Heuristic/heuristicParameterTuningAndTesting.py
@@ -0,0 +1,85 @@
+import numpy as np
+import time
+import random
+import re
+import matplotlib.pyplot as plt
+import heapq
+from operator import itemgetter
+from heuristicSolver import *
+import math
+
+from sklearn.utils.random import sample_without_replacement #########################
+
+import pandas as pd
+
+# converts rho from a expression in terms of n to a number
+def rhoToNumber(rhoString, n):
+    if rhoString == "1":
+        rho = 1
+    elif rhoString == "n^(-1/2)":
+        rho = math.pow(n, -1/2)
+    elif rhoString == "n^(-1)":
+        rho = math.pow(n, -1)
+    elif rhoString == "n^(-3/2)":
+        rho = math.pow(n, -3/2)
+    elif rhoString == "0":
+        rho = 0
+    else:
+        raise ValueError("Not a valid input for rho.")
+    return rho
+
+# tune the parameters of the algorithm for a particular instance
+def tuneForInstance(i, filename, jobDistribution):
+    X = solver()
+    X.importInstance(filename)
+    n = len(X.p)
+    m = X.m
+    paramTuningResults = []
+    for start in ["random", "greedy"]:
+        for neighbourhood in ["move", "swap"]:
+            for rho in ["1", "n^(-1/2)", "n^(-1)", "n^(-3/2)", "0"]:
+                sol = X.solve(rhoToNumber(rho, n), start, neighbourhood)
+                paramTuningResults.append([i, jobDistribution, n, m/n,  X.lowerBound, sol[1], rho, start, neighbourhood])
+    return paramTuningResults
+
+def generateTuningData(dataname):
+    kinds = ["discreteUniform", "largeJobs", "extremes", "centred"]
+    paramTuningResults = []
+    i = 0
+    for nmagnitude in range(1, 5+1):
+        for mfrac in range(1, 4+1):
+            for kind in kinds:
+                filename = str(10) + " " + str(mfrac) + " " + str(nmagnitude) + " " + kind + " " + str(1) + ".txt"
+                paramTuningResults.extend(tuneForInstance(i, "instances/" + filename, kind))
+                i += 1
+
+    df = pd.DataFrame(paramTuningResults, columns = ["id", "jobDist", "n", "mfracn", "lowerBound", "cost", "rho", "start", "neighbourhood"])
+    df.to_csv(dataname)
+    return df
+
+
+def generateTestDataForFixedTime(dataname):
+    kinds = ["discreteUniform", "largeJobs", "extremes", "centred"]
+    results = []
+    progress = 0
+    for nmagnitude in range(1, 5+1):
+        for mfrac in range(1, 4+1):
+            for kind in kinds:
+                for i in range(1, 10+1):
+                    filename = str(10) + " " + str(mfrac) + " " + str(nmagnitude) + " " + kind + " " + str(i) + ".txt"
+                    X = solver()
+                    X.importInstance("instances/" + filename)
+                    n = len(X.p)
+                    m = X.m
+                    X.allowableTime = 40 # give 40 seconds for testing on each instance
+                    sol = X.solve(math.pow(n, -3/2), "greedy", "swap")
+                    results.append([kind, n, m, X.lowerBound, sol[1], sol[2]])
+                    progress += 1
+                    print("%.2f%% complete. Estimated time remaining: %.2f minutes." % (100*progress/(5*4*4*10), X.allowableTime*(5*4*4*10 - progress)/60)) # print progress in percentage complete
+    df = pd.DataFrame(results, columns = ["jobDist", "n", "m", "lowerBound", "cost", "time"])
+    df.to_csv(dataname)
+    return df
+
+
+# COMPUTE MEAN OF COST/LOWERBOUND
+# df.loc[df['n'] < 10**5].groupby(["rho", "start", "neighbourhood"]).agg({"cost" : "mean"}).sort_values("cost")
